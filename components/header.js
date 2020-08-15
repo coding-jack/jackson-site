662ed6bc05feb1070c2c0f2d7bc2a32fb78c3850
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-08-14 22:13:44 -0600

    Changed header props to match current route

--- components/header.js
+++ components/header.js
@@ -2,13 +2,14 @@ import React from 'react'
 import useGetWPPages from '../hooks/useGetWPPages';
 import { useStore, connect } from "react-redux";
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 export default function header() {
   useGetWPPages();
   const { pages } = useStore().getState().wp
-  // TODO: Change Filter to route for props
-  const content = pages.filter((page) => page.slug === "dev")
+  const router = useRouter()
+  const content = pages.filter((page) => page.slug === router.pathname.substr(1))
 
   return (
     <header className="flex justify-center fixed inset-x-0 top-0 bg-black bg-opacity-25">
