db69f710ce58521e8d91ffb7f0befb48a9f317c9
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-08-16 22:08:28 -0600

    Pulled in dev page content

--- pages/dev.js
+++ pages/dev.js
@@ -22,27 +22,72 @@ const DevPage = () => {
           </div>
           {/* <img src={backgroundUrl} alt="" /> */}
           <div name="About" id="#About" className="section flex-col text-center">
-            <p>{content[0].acf.sections.about.header}</p>
+            <h2>{content[0].acf.sections.about.header}</h2>
             <p>{content[0].acf.sections.about.description}</p>
+            <img src={content[0].acf.sections.about.featured_image_1.sizes.medium} alt="" />
+            <img src={content[0].acf.sections.about.featured_image_2.sizes.medium} alt="" />
           </div>
-          <div name="Portfolio" id="#Portfolio" className="section flex-col text-center m-10">
-            <p>{content[0].acf.sections.portfolio.header}</p>
+          <div name="Portfolio" id="#Portfolio" className="section flex-col text-center">
+            <h2>{content[0].acf.sections.portfolio.header}</h2>
+            <p>{content[0].acf.sections.portfolio.description}</p>
             <ul>
               {content[0].acf.sections.portfolio.wordpress.map((project, index) =>
-                <li>
-                  <p key={index}>{project.image.title}</p>
+                <li key={index}>
+                  <img className="mx-auto" src={project.image.sizes.medium} alt="" />
+                  <h3>{project.image.title}</h3>
+                  <p>{project.image.description}</p>
                 </li>
               )}
             </ul>
           </div>
-          <div name="Experience" id="#Experience" className="section flex-col text-center m-10">
-            <p>{content[0].acf.sections.experience.header}</p>
+          <div name="Experience" id="#Experience" className="section flex-col text-center">
+            <h2>{content[0].acf.sections.experience.header}</h2>
+            <p>{content[0].acf.sections.experience.description}</p>
+            <ul>
+              {content[0].acf.sections.experience.jobs.map((job, index) =>
+                <li key={index}>
+                  <h3>{job.title}</h3>
+                  <p>{job.company}</p>
+                  <p>{job.location}</p>
+                  <p>{job.date_employed}</p>
+                  <p>{job.tech_used}</p>
+                  <p>{job.about}</p>
+                </li>
+              )}
+            </ul>
           </div>
-          <div name="Education" id="#Education" className="section flex-col text-center m-10">
-            <p>{content[0].acf.sections.education.header}</p>
+          <div name="Education" id="#Education" className="section flex-col text-center">
+            <h2>{content[0].acf.sections.education.header}</h2>
+            <p>{content[0].acf.sections.education.description}</p>
+            <ul>
+              {content[0].acf.sections.education.schools.map((school, index) =>
+                <li key={index}>
+                  <h3>{school.name}</h3>
+                  <p>{school.location}</p>
+                  <p>{school.date}</p>
+                  <p>{school.certification}</p>
+                  <p>{school.description}</p>
+                </li>
+              )}
+            </ul>
           </div>
-          <div name="Skills" id="#Skills" className="section flex-col text-center m-10">
-            <p>{content[0].acf.sections.skills.header}</p>
+          <div name="Skills" id="#Skills" className="section flex-col text-center">
+            <h2>{content[0].acf.sections.skills.header}</h2>
+            <p>{content[0].acf.sections.skills.description}</p>
+            <ul>
+              {content[0].acf.sections.skills.skill_sets.map((skill_set, index) =>
+                <li key={index}>
+                  <h3>{skill_set.title}</h3>
+                  <ul>
+                    {skill_set.skills.map((skill, index) =>
+                      <li key={index}>
+                        <p>{skill.skill}</p>
+                      </li>
+                    )}
+                  </ul>
+                </li>
+              )}
+            </ul>
           </div>
         </>
       }
