ca903e94ecd615257f4f7788e093efc423b83299
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-01-05 19:31:21 -0700

    Added Experience Component

---
+++ components/dev/Experience.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useStore, connect } from 'react-redux'
+
+const Experience = () => {
+  const { pages } = useStore().getState().wp
+  const content = pages.filter((page) => page.slug === "dev")
+
+  return (
+    <div name="Experience" id="#Experience" className="section flex-col text-center">
+      <h2>{content[0].acf.sections.experience.header}</h2>
+      <p className="p-12">{content[0].acf.sections.experience.description}</p>
+      <div>
+        {content[0].acf.sections.experience.jobs.map((job, index) =>
+          <div key={index} className="py-5">
+            <div className="title pb-5">
+              <h3 className="">{job.title}</h3>
+            </div>
+            <div className="grid grid-flow-col grid-cols-3 gap-4 pb-6">
+              <div className={`my-auto ${index % 2 ? 'md:order-2 md:col-span-2' : 'md:col-span-1'}`}>
+                <p>{job.company}</p>
+                <p>{job.location}</p>
+                <p>{job.date_employed}</p>
+                <p>{job.tech_used}</p>
+              </div>
+              <div className="col-span-2">
+                <p>{job.about}</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  pages: state.wp.pages,
+})
+
+const Container = connect(mapStateToProps)(Experience);
+
+export default Container;
--- pages/dev.js
+++ pages/dev.js
@@ -4,8 +4,7 @@ import { useStore, connect } from "react-redux";
 import Header from "../components/Header";
 import Hero from "../components/dev/DevHero";
 import About from "../components/dev/About";
-import Slider from "react-slick";
-import Lightbox from 'react-image-lightbox';
+import Experience from "../components/dev/Experience";
 import GalleryProjects from '../components/dev/GalleryProjects'
 
 
@@ -51,30 +50,7 @@ const DevPage = () => {
           <Hero />
           <GalleryProjects />
           <About />
-          <div name="Experience" id="#Experience" className="section flex-col text-center">
-            <h2>{content[0].acf.sections.experience.header}</h2>
-            <p className="p-12">{content[0].acf.sections.experience.description}</p>
-            <div>
-              {content[0].acf.sections.experience.jobs.map((job, index) =>
-                <div key={index} className="py-5">
-                  <div className="title pb-5">
-                    <h3 className="">{job.title}</h3>
-                  </div>
-                  <div className="grid grid-flow-col grid-cols-3 gap-4 pb-6">
-                    <div className={`my-auto ${index % 2 ? 'md:order-2 md:col-span-2' : 'md:col-span-1'}`}>
-                      <p>{job.company}</p>
-                      <p>{job.location}</p>
-                      <p>{job.date_employed}</p>
-                      <p>{job.tech_used}</p>
-                    </div>
-                    <div className="col-span-2">
-                      <p>{job.about}</p>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+          <Experience />
           <div name="Education" id="#Education" className="section flex-col text-center">
             <h2>{content[0].acf.sections.education.header}</h2>
             <p className="p-12">{content[0].acf.sections.education.description}</p>
