00f29f6fc1cf8d65b5ca7dc923e4a8c81b8df4b0
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-02-01 21:19:44 -0700

    Added nav to dev

--- pages/dev.js
+++ pages/dev.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useStore, connect } from "react-redux";
 import Layout from "../components/Layout";
 import Hero from "../components/dev/DevHero";
-import About from "../components/dev/About";
 import Experience from "../components/dev/Experience";
 import Education from "../components/dev/Education";
 import Skills from "../components/dev/Skills";
 import GalleryProjects from '../components/dev/GalleryProjects'
+import Header from "../components/Header"
 
 
 const DevPage = () => {
@@ -17,6 +17,7 @@ const DevPage = () => {
     <Layout className="dev">
       {content[0] &&
         <>
+          <Header />
           <Hero />
           <GalleryProjects />
           <Experience />
