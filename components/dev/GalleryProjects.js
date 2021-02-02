fc7c1ff578aa5589c5eaceb12c298be2ca73f543
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-02-01 21:56:03 -0700

    Got rid of framer on project page

--- components/dev/GalleryProjects.js
+++ components/dev/GalleryProjects.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useStore, connect } from "react-redux";
-import { motion, useViewportScroll, AnimatePresence, AnimateSharedLayout, useAnimation } from "framer-motion";
 
 const GalleryProjects = () => {
   const { pages } = useStore().getState().wp
@@ -9,7 +8,6 @@ const GalleryProjects = () => {
     minHeight: '180px',
   }
   const [selectedId, setSelectedId] = useState(null)
-  const { scrollYProgress } = useViewportScroll()
 
   return (
     <div name="Portfolio" id="#Portfolio" className="flex-col min-h-screen p-20 text-center">
@@ -18,65 +16,41 @@ const GalleryProjects = () => {
       <div className="wp-project-gallery">
         <h3>Wordpress</h3>
         <div style={divStyle} className="grid md:grid-cols-3 sm:grid-cols-2 gap-6 mb-3">
-          <AnimateSharedLayout type="crossfade">
-            {content[0].acf.sections.portfolio.wordpress
-              .slice(0, 3)
-              .map((project, index) =>
-                <motion.div
-                  key={index}
-                  animate={{ rotate: 360, scale: 1 }}
-                  layoutId={project.index}
-                  onClick={() => setSelectedId(project.index)}
-                  transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20
-                  }}
-                  className={`gallery-photo rounded-lg border border-red bg-top bg-cover cursor-pointer relative ${index === 2 ? 'sm:hidden md:block' : ''}`}
-                  style={{ backgroundImage: `url('${project.image.url}')` }}
-                  alt={project.image.alt}
-                />
-              )}
-            <AnimatePresence>
-              {selectedId && (
-                <motion.div layoutId={selectedIdentifier}>
-                  <motion.button onClick={() => setSelectedId(null)} />
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </AnimateSharedLayout>
+          {content[0].acf.sections.portfolio.wordpress
+            .slice(0, 3)
+            .map((project, index) =>
+              <div
+                key={index}
+                className={`gallery-photo rounded-lg border border-red bg-top bg-cover cursor-pointer relative ${index === 2 ? 'sm:hidden md:block' : ''}`}
+                style={{ backgroundImage: `url('${project.image.url}')` }}
+                alt={project.image.alt}
+              />
+            )}
+          {selectedId && (
+            <div layoutId={selectedIdentifier}>
+              <button onClick={() => setSelectedId(null)} />
+            </div>
+          )}
         </div>
       </div>
       <div className="js-project-gallery">
         <h3>Javascript</h3>
         <div style={divStyle} className="grid md:grid-cols-3 sm:grid-cols-2 gap-6 mb-3">
-          <AnimateSharedLayout type="crossfade">
-            {content[0].acf.sections.portfolio.javascript
-              .slice(0, 3)
-              .map((project, index) =>
-                <motion.div
-                  key={index}
-                  animate={{ rotate: 360, scale: 1 }}
-                  layoutId={project.index}
-                  onClick={() => setSelectedId(project.index)}
-                  transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20
-                  }}
-                  className={`gallery-photo rounded-lg border border-red bg-top bg-cover cursor-pointer ${index === 2 ? 'sm:hidden md:block' : ''}`}
-                  style={{ backgroundImage: `url('${project.image.url}')` }}
-                  alt={project.image.alt}
-                />
-              )}
-            <AnimatePresence>
-              {selectedId && (
-                <motion.div layoutId={selectedIdentifier}>
-                  <motion.button onClick={() => setSelectedId(null)} />
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </AnimateSharedLayout>
+          {content[0].acf.sections.portfolio.javascript
+            .slice(0, 3)
+            .map((project, index) =>
+              <div
+                key={index}
+                className={`gallery-photo rounded-lg border border-red bg-top bg-cover cursor-pointer ${index === 2 ? 'sm:hidden md:block' : ''}`}
+                style={{ backgroundImage: `url('${project.image.url}')` }}
+                alt={project.image.alt}
+              />
+            )}
+          {selectedId && (
+            <div layoutId={selectedIdentifier}>
+              <button onClick={() => setSelectedId(null)} />
+            </div>
+          )}
         </div>
       </div>
     </div>
