a9783a7405b7550d502939110d87e91ddfa4ae98
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-02-12 18:54:09 -0700

    Trying different webpack config solution for netlify build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   images: {
     domains: ['wp.jacksonkaufman.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  },
 }
