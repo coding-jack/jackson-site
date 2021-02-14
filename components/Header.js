80a109668c93881ab0cae8709ad0f64d46caff9d
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-02-13 20:09:06 -0700

    Changed Header items boldness

--- components/Header.js
+++ components/Header.js
@@ -41,7 +41,7 @@ function Header() {
                       {Object.values(content[0].acf.sections).map((section) =>
                         <div className="px-3" key={section.header}>
                           <Link href={`#${removeSpace(section.header)}`}>
-                            <a className="text-primary font-black">{section.header}</a>
+                            <a className="text-primary font-bold">{section.header}</a>
                           </Link>
                         </div>
                       )}
